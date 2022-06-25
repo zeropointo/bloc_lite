b50ed054c858ba64464ba64ff52a186f56a6dde2
Robert Bennett <robert.bennett@ifrontiers.com>
2022-06-25 12:14:03 -0400

    Make BlocBuilder generic.

--- lib/bloc_builder.dart
+++ lib/bloc_builder.dart
@@ -3,7 +3,7 @@ part of 'bloc_lite.dart';
 typedef BlocStateBuilder<StateT> = Widget Function(
     BuildContext context, StateT state);
 
-abstract class BlocBuilder extends StatelessWidget {
+class BlocBuilder<BlocT> extends StatelessWidget {
   const BlocBuilder({Key? key, required this.context, required this.builder})
       : super(key: key);
 
@@ -12,7 +12,7 @@ abstract class BlocBuilder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Bloc bloc = BlocProvider.of(context).bloc;
+    Bloc bloc = BlocProvider.of<BlocT>(context).bloc;
 
     return StreamBuilder<BlocState>(
         stream: bloc.stateStream,
